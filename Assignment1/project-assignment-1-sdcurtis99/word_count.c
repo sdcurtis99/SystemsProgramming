6432f0a98940b737858aa8b5722dda72e8c14b25
Shannon Curtis <scurti03@unix5.csc.calpoly.edu>
2026-01-31 16:19:32 -0800

    Fixed bug in word count function

--- Assignment1/project-assignment-1-sdcurtis99/word_count.c
+++ Assignment1/project-assignment-1-sdcurtis99/word_count.c
@@ -3,13 +3,7 @@
  *
  * It should take a single optional cml argument specfiying a file name. It must read the content of the files or from stdin and print .... . Use a single helper function to implment the function required.
  *
- * Note the program should not use any form od dyanmic memmoery allocation. It should read a SINGLE CHARACTER at a time. Multiple white spaces should be treated as a single ws delimiter.
- *
- */
-
-
-:
-/*
+ * Note the program should not use any form od dyanmic memmoery allocation. It should read a SINGLE CHARACTER at a time. Multiple white spaces should be treated as a single ws delimiter
  *	ToDo:
  *		Implment wc logic into a function
  *		Check logic is correct
@@ -19,29 +13,30 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #define LENGTH 3
 
-void  wcResults(int* array, int argc, FILE* fp) {
-	char ch;
+void  wcResults(int resArr[], int argc, FILE* fp) {
+	int ch;
 	int inWord = 0;
 	int chars = 0;
 	int lines = 0;
 	int words = 0;
-	while( ( (argc == 2 && fp != NULL) ? (ch = fgetc(fp) != EOF) : (scanf("%c", &ch) == 1) ) ) {
-		if (ch = ' ') {
+	while(((argc == 2 && fp != NULL) ? ((ch = fgetc(fp)) != EOF) : ((ch = fgetc(stdin)) != EOF))) {
+		if (ch == ' ' || ch == '\n' || ch == '\t') {
 			inWord = 0;	
 		}
-		if ((ch != ' ') && inWord = 0) {
+		else if (!inWord) {
 			inWord = 1;
-			word++
+			words++;
 		}	
 		chars++;
 		if (ch == '\n') {lines++;}
 	}
-	arr[0] = lines;
-	arr[1] = words;
-	arr[2] = chars;	
-	return 0;
+	resArr[0] = lines;
+	resArr[1] = words;
+	resArr[2] = chars;	
+	return;
 }
 
 int main(int argc, char* argv[])  {
@@ -50,10 +45,19 @@ int main(int argc, char* argv[])  {
 		printf("Invalid number of cml arguments used in executable call");	
 		return -1;
 	}
-	char* fileName = argv[1]
-	int* resArr =  (int *) malloc(sizeof(int) * LENGTH);
+	FILE* fp = NULL;
+	char* fileName = NULL; 
+	if (argc == 2) {
+		fileName = argv[1];
+		fp = fopen(fileName, "r");
+		if (!fp) {
+			printf("File could not be opened/does not exist.");
+			return -1;
+		}
+	}
+	int resArr[LENGTH];
 	wcResults(resArr, argc, fp);
-	printf("/t%d/t%d/t%d %s\n", resArr[0], resArr[1], resArr[2], fileName);	
+	fp ? printf("\t%d\t%d\t%d %s\n", resArr[0], resArr[1], resArr[2], fileName) : printf("\t%d\t%d\t%d\n", resArr[0], resArr[1], resArr[2]);
 	return 1;
 }
 
