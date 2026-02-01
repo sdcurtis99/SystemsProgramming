aa3f7d510c7fd080b64799c7040d3c8fdaf680ae
Shannon Curtis <scurti03@unix5.csc.calpoly.edu>
2026-01-31 16:19:32 -0800

    Fixed bug in word count function

---
+++ Assignment1/project-assignment-1-sdcurtis99/uniq.c
@@ -0,0 +1,10 @@
+/*
+ * Write the unqie utiltiy
+ * It echoes to stdout its input but will only print a single instance of consequtive mathcing lines.
+ *
+ * Write a program with main() that takes a single cml argument specifying the file name. Read in the
+ * contents of file or from stdin if not specfied and print only consequite unqiue lines to stdout.
+ *
+ * Use a single helper function to implment this behavior.
+ *
+ *
